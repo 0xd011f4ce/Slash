cbd93e2925a1401c3fc6bdead89016ce64d65cdf
lain <lyx@lyxincyberia.org>
2024-10-02 14:13:46 -0500

    drawing debug spheres

--- Source/Slash/Private/Items/Item.cpp
+++ Source/Slash/Private/Items/Item.cpp
@@ -1,6 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Items/Item.h"
+#include "DrawDebugHelpers.h"
+
+#define DRAW_SPHERE(Location)                                                 \
+  if (GetWorld ())                                                            \
+    DrawDebugSphere (GetWorld (), Location, 25.f, 12, FColor::Red, true);
 
 AItem::AItem () { PrimaryActorTick.bCanEverTick = true; }
 
@@ -13,6 +18,13 @@ AItem::BeginPlay ()
     {
       GEngine->AddOnScreenDebugMessage (1, 60.f, FColor::Cyan,
                                         TEXT ("We are using item!"));
+    }
+
+  UWorld *World = GetWorld ();
+  if (World)
+    {
+      FVector Location = GetActorLocation ();
+      DRAW_SPHERE (Location)
     }
 }
 
